4ad62bedad5895e04928b1017a9ba49ca80d2a73
Kirill Golovin <golovin91k@gmail.com>
2024-01-06 01:55:02 +0300

    s

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from ice_cream.models import IceCream
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    #template = 'homepage/index.html'
+    #return render(request, template)
+    template_name = 'homepage/index.html'
+    # Заключаем вызов методов в скобки
+    # (это стандартный способ переноса длинных строк в Python);
+    # каждый вызов пишем с новой строки, так проще читать код:
+    ice_cream_list = IceCream.objects.values(
+            'id', 'title', 'description').filter(is_on_main=True)
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context) 
